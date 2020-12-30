6e1b40ff30036b906ca99537dc603206945782d9
samuelecisaro <samuele.cisaro@hotmail.it>
2020-12-30 07:44:17 +0100

    refactor: change function to get weekday name

--- bikeshare.py
+++ bikeshare.py
@@ -60,7 +60,7 @@ def load_data(city, month, day):
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     
     #create three new columns for week day, month and hour
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
+    df['day_of_week'] = df['Start Time'].dt.day_name()
     df['month'] = df['Start Time'].dt.month
     df['hour'] = df['Start Time'].dt.hour
     
